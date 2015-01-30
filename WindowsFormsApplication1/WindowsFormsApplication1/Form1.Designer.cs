317a41ca88a101dc1436b14b03a307e34462ecc7
incapsulate <incapsulate@gmail.com>
2015-01-30 14:17:44 +0200

    test_browser

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(849, 655);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webControl1
+            // 
+            this.webControl1.Location = new System.Drawing.Point(80, 12);
+            this.webControl1.Size = new System.Drawing.Size(717, 460);
+            this.webControl1.Source = new System.Uri("about:blank", System.UriKind.Absolute);
+            this.webControl1.TabIndex = 2;
+            this.webControl1.JavascriptRequest += new Awesomium.Core.JavascriptRequestEventHandler(this.Awesomium_Windows_Forms_WebControl_JavascriptRequest);
+            this.webControl1.JavascriptMessage += new Awesomium.Core.JavascriptMessageEventHandler(this.Awesomium_Windows_Forms_WebControl_JavascriptMessage);
+            this.webControl1.ShowCreatedWebView += new Awesomium.Core.ShowCreatedWebViewEventHandler(this.Awesomium_Windows_Forms_WebControl_ShowCreatedWebView);
+            this.webControl1.DocumentReady += new Awesomium.Core.DocumentReadyEventHandler(this.Awesomium_Windows_Forms_WebControl_DocumentReady);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(712, 553);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(762, 591);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(80, 503);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(626, 127);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 655);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private Awesomium.Windows.Forms.WebControl webControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Skybound;
+using Skybound.Gecko;
+using Awesomium.Core;
+using System.Net;
+using System.IO;
+//using Awesomium.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private Awesomium.Core.JSValue x;
+        private WebView webView;
+        private WebBrowser browser = new WebBrowser();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        public void HElloMEs()
+        {
+            MessageBox.Show("asd");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webControl1.Source = new Uri("C:\\Users\\asdfghj\\Desktop\\test_browser\\index.html");            //      Рабочий Код
+
+            //timer1.Enabled = true;
+            //x = webControl1.ExecuteJavascriptWithResult("alert(msg)");
+            //label1.Text = x.ToString();
+            //richTextBox1.Text = webControl1.IsDocumentReady.ToString();
+            if (webControl1.IsDocumentReady)
+            {
+                //x = webControl1.ExecuteJavascriptWithResult("test();");
+                //label1.Text = x.ToString();
+                //x = webControl1.ExecuteJavascriptWithResult("ask_js");
+                //we
+                //richTextBox1.Text = x.ToString();
+                //x = webControl1.ExecuteJavascriptWithResult("document.getElementById('response')");
+                //label1.Text = x.ToString();
+                //browser.Document.InvokeScript("HElloMEs", new String[] { "called from client code" });
+            }
+            
+            //x = webControl1.ExecuteJavascriptWithResult("data.message", "100");
+            //textBox1.Text = x.ToString();
+              //x = webControl1.ExecuteJavascriptWithResult("channel");
+
+            //webControl1.ExecuteJavascript("channel");
+            
+            //textBox1.Text = x.ToString();
+            //richTextBox1.Text = webControl1.HTML;
+            //WebRequest request = WebRequest.Create("http://app.pusherapp.com/apps/102936");
+            //WebRequest request = WebRequest.Create("http://api.pusherapp.com/apps/102936/events?name=myevent&channel=test_channel&body_md5=8a3501faef6636ca9a5ebbe6f31b5409&auth_version=1.0&auth_key=838aaf1cb859e6951945&auth_timestamp=1421243531&auth_signature=bdeeb80fff229203e421c19a5bbafc2666dbdba72c85712ab3f4bd19638d5e3e");
+        
+            /*WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string res = reader.ReadToEnd();*/
+            //textBox1.Text = res;
+            //richTextBox1.Text = res;
+
+            //WebView asd = new WebView();
+            //webView.LoadFile("C:\\Users\\asdfghj\\Desktop\\test_browser\\index.html");
+            //WebView wtf = new WebView(500,500);
+            
+            /*WebView wtf = WebCore.CreateWebView(500, 500);
+            if (wtf.IsDocumentReady)
+            {
+                //wtf.LoadingFrameComplete += wtf.ExecuteJavascript;
+                wtf.Source = new Uri("C:\\Users\\asdfghj\\Desktop\\test_browser\\index.html");
+                //JSValue param1 = new JSValue("my_event");
+                JSValue res = wtf.ExecuteJavascriptWithResult("my_event");
+                richTextBox1.Text = res.ToString();
+            }*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //richTextBox1.Text = webControl1.IsDocumentReady.ToString();
+            //x = webControl1.ExecuteJavascriptWithResult("test();");               //      Рабочий код
+            //label1.Text = x.ToString();
+            
+            
+            
+            
+            
+            //richTextBox1.Text = webControl1.HTML;
+            //label1.Text = x;
+            //label1.Text = webControl1.ExecuteJavascriptWithResult("data.message", "100");
+            //webControl1.
+            //ResourceRequest pf = new ResourceRequest();
+            //pf.
+            //WebView webView;
+            //timer1.Enabled = true;
+
+            //webView.Source = new Uri("C:\\Users\\asdfghj\\Desktop\\test_browser\\index.html");
+
+            
+            
+
+            /*webView.DocumentReady += WebViewOnDocumentReady;
+
+            webView.Source = new Uri("C:\\Users\\asdfghj\\Desktop\\test_browser\\index.html");*/
+
+        }
+
+        
+        /*private void WebViewOnDocumentReady(object sender, UrlEventArgs urlEventArgs)
+        {
+            webView.DocumentReady -= WebViewOnDocumentReady;
+
+            JSObject jsobject = webView.CreateGlobalJavascriptObject("jsobject");
+
+            //jsobject.Bind("callNETNoReturn", false, JSHandler);
+            //jsobject.Bind("callNETWithReturn", true, JSHandler);
+            jsobject.Bind();
+
+            webView.ExecuteJavascript("myMethod()");
+            webView.ExecuteJavascript("myMethodExpectingReturn()");
+            var result = webView.ExecuteJavascriptWithResult("myMethodProvidingReturn('foo')");
+            Console.WriteLine(result.ToString());
+        }
+
+        private void JSHandler(object sender, JavascriptMethodEventArgs args)
+        {
+            if (args.MustReturnValue)
+            {
+                Console.WriteLine("Got method call with return request");
+                args.Result = "Returning " + args.Arguments[0];
+            }
+            else
+            {
+                Console.WriteLine("Got method call with no return request");
+            }
+        }*/
+
+        private void Awesomium_Windows_Forms_WebControl_ShowCreatedWebView(object sender, ShowCreatedWebViewEventArgs e)
+        {
+
+        }
+
+        private void Awesomium_Windows_Forms_WebControl_JavascriptRequest(object sender, JavascriptRequestEventArgs e)
+        {
+
+        }
+
+        private void Awesomium_Windows_Forms_WebControl_DocumentReady(object sender, DocumentReadyEventArgs e)
+        {
+            //x = webControl1.ExecuteJavascriptWithResult("document.getElementById('response').textContent");
+            //x = webControl1.ExecuteJavascriptWithResult("test();");
+            //label1.Text = x.ToString();
+            timer1.Enabled = true;
+        }
+
+        private void Awesomium_Windows_Forms_WebControl_JavascriptMessage(object sender, JavascriptMessageEventArgs e)
+        {
+            //x = webControl1.ExecuteJavascriptWithResult("alert(message)");
+            //label1.Text = x.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //x = webControl1.ExecuteJavascriptWithResult("test();");
+            //label1.Text = x.ToString();
+            
+            richTextBox1.Text = webControl1.IsDocumentReady.ToString();
+            if (webControl1.IsDocumentReady)
+            {
+
+                x = webControl1.ExecuteJavascriptWithResult("test();");
+                label1.Text = x.ToString();
+            }
+        }
+
+
+       
+
+    }
+}
